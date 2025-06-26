a6e21e83ee8a7a86ec6980e152e8fc5b8182a283
Mohammad Shams <mamad.sh96@gmail.com>
2025-06-26 13:32:51 +0330

    fix test handler

--- QuizoDotnet/Handlers/RequestHandlers/TestRequestHandler.cs
+++ QuizoDotnet/Handlers/RequestHandlers/TestRequestHandler.cs
@@ -1,14 +1,15 @@
 using KarizmaPlatform.Connection.Server.Attributes;
+using KarizmaPlatform.Connection.Server.Base;
 
 namespace QuizoDotnet.Handlers.RequestHandlers;
 
 [RequestHandler("/test")]
-public class TestRequestHandler
+public class TestRequestHandler : BaseRequestHandler
 {
-    [Action("get-test")]
+    [Action("get-test", needAuthorizedUser: false)]
     public async Task<string> GetTest()
     {
-        await Task.Delay(3000);
+        await Task.Delay(1000);
         return "Test";
     }
 }
